77ece4cb7bae943a571cc903c17678a38a96a3c9
MrShadow <mrshadowccg@hotmail.com>
2024-07-07 18:02:00 -0300

    Initial commit

---
+++ system/scripts/zone/content/r1/npcs/goddess_statues.cs
@@ -0,0 +1,19 @@
+﻿//--- Melia Script ----------------------------------------------------------
+// GoddessStatues
+//--- Description -----------------------------------------------------------
+// Goddess Statues NPCs around the world.
+//---------------------------------------------------------------------------
+
+using Melia.Shared.Game.Const;
+using Melia.Zone.Scripting;
+using static Melia.Zone.Scripting.Shortcuts;
+
+public class GoddessStatuesNpcScript : GeneralScript
+{
+	public override void Load()
+	{
+		// Klaipeda
+		//-------------------------------------------------------------------------
+		AddNpc(10017, 154039, "Statue of Goddess Ausrine", "c_Klaipe", -206.574, 148.8251, 98.63973, 45, "WARP_C_KLAIPE", "STOUP_CAMP", "STOUP_CAMP");
+	}
+}
