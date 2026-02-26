7e1bd3366555197b45030529ac7f42adfcb0d958
kenedos <mrshadowccg@hotmail.com>
2026-02-26 19:28:59 -0300

    Chronomancer reincarnate fixes

--- src/ZoneServer/Packages/Laima/Skills/Wizards/Chronomancer/Chronomancer_Samsara.cs
+++ src/ZoneServer/Packages/Laima/Skills/Wizards/Chronomancer/Chronomancer_Samsara.cs
@@ -1,16 +1,16 @@
 using System;
-using System.Threading.Tasks;
-using Melia.Shared.Packages;
+using System.Collections.Generic;
+using System.Linq;
 using Melia.Shared.Game.Const;
+using Melia.Shared.L10N;
+using Melia.Shared.Packages;
 using Melia.Shared.World;
 using Melia.Zone.Network;
-using Melia.Zone.World.Actors.Characters;
-using Melia.Zone.World.Actors;
 using Melia.Zone.Skills.Handlers.Base;
-using Melia.Zone.World.Actors.CombatEntities.Components;
-using Melia.Zone.Buffs;
-using Melia.Shared.L10N;
-using System.Linq;
+using Melia.Zone.World.Actors;
+using Melia.Zone.World.Actors.Characters;
+using Melia.Zone.World.Actors.Monsters;
+using Yggdrasil.Util;
 
 namespace Melia.Zone.Skills.Handlers.Wizards.Chronomancer
 {
@@ -18,6 +18,9 @@ namespace Melia.Zone.Skills.Handlers.Wizards.Chronomancer
 	[SkillHandler(SkillId.Chronomancer_Samsara)]
 	public class Chronomancer_SamsaraOverride : IMeleeGroundSkillHandler
 	{
+		private const string VarReincarnated = "Melia.Skill.Samsara.Reincarnated";
+		private const string VarCreatedBySamsara = "Melia.Skill.Samsara.Created";
+
 		public void Handle(Skill skill, ICombatEntity caster, Position originPos, Position farPos, params ICombatEntity[] targets)
 		{
 			if (!caster.TrySpendSp(skill))
@@ -27,9 +30,11 @@ namespace Melia.Zone.Skills.Handlers.Wizards.Chronomancer
 			}
 
 			skill.IncreaseOverheat();
-			var designatedTarget = targets?.FirstOrDefault();
-			caster.TurnTowards(designatedTarget);
+			caster.TurnTowards(farPos);
 			caster.SetAttackState(true);
+
+			var maxTargets = 3 + skill.Level / 2;
+			var deadTargets = GetDeadEnemies(caster, farPos, maxTargets);
 
 			var skillHandle = ZoneServer.Instance.World.CreateSkillHandle();
 
@@ -37,14 +42,54 @@ namespace Melia.Zone.Skills.Handlers.Wizards.Chronomancer
 			Send.ZC_NORMAL.UpdateSkillEffect(caster, caster.Handle, caster.Position, caster.Direction, caster.Position);
 
 			Send.ZC_SYNC_START(caster, skillHandle, 1);
-			if (designatedTarget != null && designatedTarget.IsDead)
+			foreach (var deadMob in deadTargets)
 			{
-				designatedTarget.StartBuff(BuffId.Samsara_Buff, TimeSpan.FromSeconds(1), caster);
+				deadMob.Vars.SetBool(VarReincarnated, true);
+
+				var createCount = 1;
+				if (caster is Character character
+					&& character.TryGetActiveAbilityLevel(AbilityId.Chronomancer3, out var abilityLevel)
+					&& RandomProvider.Next(1, 101) <= abilityLevel)
+				{
+					createCount++;
+				}
+
+				for (var i = 0; i < createCount; i++)
+				{
+					if (!deadMob.Map.TryGetRandomPositionInRange(deadMob.Position, 10, out var position))
+						position = deadMob.Position.GetRandomInRange2D(10);
+
+					var clone = Mob.CopyFrom(deadMob, position);
+					if (clone == null)
+						continue;
+
+					var hpRate = Math.Max(0.50f, 1f - 0.03f * skill.Level);
+				clone.Properties.SetFloat(PropertyName.HP, (int)(clone.Properties.GetFloat(PropertyName.MHP) * hpRate));
+					clone.Vars.SetBool(VarCreatedBySamsara, true);
+
+					deadMob.Map.AddMonster(clone);
+
+					clone.StartBuff(BuffId.Samsara_Buff, TimeSpan.FromSeconds(1), caster);
+				}
 			}
 			Send.ZC_SYNC_END(caster, skillHandle, 0);
 			Send.ZC_SYNC_EXEC_BY_SKILL_TIME(caster, skillHandle, TimeSpan.FromMilliseconds(300));
 
 			Send.ZC_SKILL_MELEE_GROUND(caster, skill, farPos);
+		}
+
+		private static List<Mob> GetDeadEnemies(ICombatEntity caster, Position targetPos, int maxTargets)
+		{
+			const float Range = 300;
+
+			var deadMobs = caster.Map.GetActorsInRange<Mob>(targetPos, Range, mob =>
+				mob.IsDead
+				&& caster.CheckRelation(mob, RelationType.Enemy)
+				&& !mob.Vars.GetBool(VarReincarnated)
+				&& !mob.Vars.GetBool(VarCreatedBySamsara)
+			);
+
+			return deadMobs.Take(maxTargets).ToList();
 		}
 	}
 }
