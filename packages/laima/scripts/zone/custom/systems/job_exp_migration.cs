8dbf2aad9f60b02075bd2d6cdb154bce62e15191
kenedos <mrshadowccg@hotmail.com>
2026-03-17 17:10:05 -0300

    Job exp missed migration

--- packages/laima/scripts/zone/custom/systems/job_exp_migration.cs
+++ packages/laima/scripts/zone/custom/systems/job_exp_migration.cs
@@ -13,23 +13,23 @@ public class JobExpMigrationScript : GeneralScript
 		var character = args.Character;
 		var vars = character.Variables.Perm;
 	
-		if (vars.GetBool("Laima.Custom.JobExpMigration_2026_02_12", false))
+		if (vars.GetBool("Laima.Custom.JobExpMigration_2026_03_17", false))
 			return;
 	
 		character.ResetSkills();
 	
 		// Stat point migration: stats_per_level changed from 1 to 3.
 		// Grant the missing (level - 1) * 2 stat points and reset stats.
-		var level = character.Level;
-		var missingStatPoints = (level - 1) * 2;
-	
-		if (missingStatPoints > 0)
-			character.Properties.Modify(PropertyName.StatByLevel, missingStatPoints);
-	
-		character.ResetStats();
+		// var level = character.Level;
+		// var missingStatPoints = (level - 1) * 2;
+		// 
+		// if (missingStatPoints > 0)
+		// 	character.Properties.Modify(PropertyName.StatByLevel, missingStatPoints);
+		// 
+		// character.ResetStats();
 	
 		Send.ZC_OBJECT_PROPERTY(character);
 	
-		vars.SetBool("Laima.Custom.JobExpMigration_2026_02_12", true);
+		vars.SetBool("Laima.Custom.JobExpMigration_2026_03_17", true);
 	}
 }
