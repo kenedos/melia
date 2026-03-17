ee80c40e3ab7ca545944380022ceec48d580b05c
kenedos <mrshadowccg@hotmail.com>
2026-03-17 00:32:51 -0300

    Refactor reset NPC

---
+++ packages/laima/scripts/zone/custom/npcs/stat_skill_reset.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Threading.Tasks;
+using Melia.Shared.Game.Const;
+using Melia.Zone;
+using Melia.Zone.Scripting;
+using Melia.Zone.Scripting.Dialogues;
+using Melia.Zone.World.Actors.Characters;
+using static Melia.Zone.Scripting.Shortcuts;
+
+public class CustomNpcStatSkillReset : GeneralScript
+{
+	private const int VisItemId = ItemId.Vis;
+
+	private const int BasePrice = 100000;
+	private const int MaxPrice = 1600000;
+
+	protected override void Load()
+	{
+		AddNpc(57582, L("[Angel] Seraphina"), "c_Klaipe", 295, 83, 90, this.SerafinaDialog);
+		AddNpc(57582, L("[Angel] Seraphina"), "c_fedimian", -410, -262, 45, this.SerafinaDialog);
+		AddNpc(57582, L("[Angel] Seraphina"), "c_orsha", -65, 232, 90, this.SerafinaDialog);
+	}
+
+	private async Task SerafinaDialog(Dialog dialog)
+	{
+		var player = dialog.Player;
+
+		dialog.SetTitle("Seraphina");
+
+		var statResetCount = player.Variables.Perm.GetInt("StatResetCount");
+		var skillResetCount = player.Variables.Perm.GetInt("SkillResetCount");
+		var statPrice = CalculatePrice(statResetCount);
+		var skillPrice = CalculatePrice(skillResetCount);
+
+		var statPriceStr = FormatSilver(statPrice);
+		var skillPriceStr = FormatSilver(skillPrice);
+
+		var greeting = "Greetings, adventurer. I am Seraphina, an angel of the goddess Laima. "
+			+ "I can help you redistribute your stat points or reset your skill points, "
+			+ "for a fee that increases with each use.\n\n"
+			+ LF("Stat Reset: {0} Silver (reset #{1})\n", statPriceStr, statResetCount + 1)
+			+ LF("Skill Reset: {0} Silver (reset #{1})\n\n", skillPriceStr, skillResetCount + 1)
+			+ "The cost doubles each time you reset, up to a maximum of 1,600,000 Silver.";
+
+		var selection = await dialog.Select(greeting,
+			Option(LF("Stat Reset ({0} Silver)", statPriceStr), "stat_reset"),
+			Option(LF("Skill Reset ({0} Silver)", skillPriceStr), "skill_reset"),
+			Option(L("How does this work?"), "explain"),
+			Option(L("Nothing, thank you."), "cancel")
+		);
+
+		switch (selection)
+		{
+			case "stat_reset":
+				await this.HandleReset(dialog, player, true, statPrice, statResetCount);
+				break;
+			case "skill_reset":
+				await this.HandleReset(dialog, player, false, skillPrice, skillResetCount);
+				break;
+			case "explain":
+				await this.HandleExplanation(dialog);
+				break;
+			case "cancel":
+				await dialog.Msg("May the blessings of the goddess Laima be with you.");
+				break;
+		}
+	}
+
+	private async Task HandleExplanation(Dialog dialog)
+	{
+		await dialog.Msg(
+			"Here's how my services work:\n\n"
+			+ "Stat Reset: All your stat points (STR, CON, INT, SPR, DEX) will be "
+			+ "returned to you as unallocated points, allowing you to redistribute them.\n\n"
+			+ "Skill Reset: All skill points across every job class will be refunded, "
+			+ "so you can reallocate them as you see fit.\n\n"
+			+ "The cost doubles each time, up to a maximum of 1,600,000 Silver:\n"
+			+ "  Reset #1: 100,000\n"
+			+ "  Reset #2: 200,000\n"
+			+ "  Reset #3: 400,000\n"
+			+ "  Reset #4: 800,000\n"
+			+ "  Reset #5+: 1,600,000"
+		);
+	}
+
+	private async Task HandleReset(Dialog dialog, Character player, bool isStatReset, int price, int resetCount)
+	{
+		var resetTypeName = isStatReset ? "stat" : "skill";
+		var resetCountVariable = isStatReset ? "StatResetCount" : "SkillResetCount";
+
+		var confirmMsg = LF("A {0} reset will cost {1} Silver.\nYou have used this service {2} time(s) before.", resetTypeName, FormatSilver(price), resetCount);
+
+		if (price >= MaxPrice)
+			confirmMsg += "\nYou have reached the maximum price. Further resets will not increase in cost.";
+
+		if (!player.HasSilver(price))
+		{
+			await dialog.Msg(LF("{0}\n\nYou don't have enough Silver. You need {1} Silver.", confirmMsg, FormatSilver(price)));
+			return;
+		}
+
+		var confirm = await dialog.Select(confirmMsg,
+			Option(LF("Yes, reset my {0}s", resetTypeName), "confirm"),
+			Option(L("No, nevermind"), "cancel")
+		);
+
+		if (confirm == "confirm")
+		{
+			player.RemoveItem(VisItemId, price);
+
+			if (isStatReset)
+			{
+				player.ResetStats();
+			}
+			else
+			{
+				player.ResetSkills();
+				player.AddonMessage(AddonMessage.RESET_SKL_UP);
+			}
+
+			player.Variables.Perm.Set(resetCountVariable, resetCount + 1);
+
+			var nextPrice = CalculatePrice(resetCount + 1);
+			await dialog.Msg(LF("Your {0}s have been reset successfully.\n\nYour next {0} reset will cost {1} Silver.", resetTypeName, FormatSilver(nextPrice)));
+		}
+		else
+		{
+			await dialog.Msg("Perhaps another time, then.");
+		}
+	}
+
+	private static int CalculatePrice(int resetCount)
+	{
+		var price = (long)(BasePrice * Math.Pow(2, resetCount));
+
+		if (price > MaxPrice)
+			price = MaxPrice;
+
+		return (int)price;
+	}
+
+	private static string FormatSilver(int amount)
+	{
+		return amount.ToString("N0");
+	}
+}
