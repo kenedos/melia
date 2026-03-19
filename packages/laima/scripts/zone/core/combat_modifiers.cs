d53a29b46c5c57e005e30d537a04f0fa824c26fd
kenedos <mrshadowccg@hotmail.com>
2026-03-19 17:08:48 -0300

    Fix melia -> laima merge not calling abilities and buffs correctly

--- packages/laima/scripts/zone/core/combat_modifiers.cs
+++ packages/laima/scripts/zone/core/combat_modifiers.cs
@@ -5,6 +5,7 @@
 // combatants's buffs, skills, abilities, and other properties.
 //---------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Linq;
 using Melia.Shared.Game.Const;
 using Melia.Zone.Scripting;
@@ -98,6 +99,7 @@ public class CombatModifierCalculationsScript : GeneralScript
 	/// <param name="skillHitResult"></param>
 	private void CallAll(string baseFuncName, ICombatEntity attacker, ICombatEntity target, Skill skill, SkillModifier modifier, SkillHitResult skillHitResult)
 	{
+		// Old System
 		CallForBuffs(baseFuncName, attacker, attacker, target, skill, modifier, skillHitResult);
 		CallForBuffs(baseFuncName, target, attacker, target, skill, modifier, skillHitResult);
 
@@ -106,6 +108,11 @@ public class CombatModifierCalculationsScript : GeneralScript
 
 		CallForAbilities(baseFuncName, attacker, attacker, target, skill, modifier, skillHitResult);
 		CallForAbilities(baseFuncName, target, attacker, target, skill, modifier, skillHitResult);
+
+		// New System
+		CallForBuffs(baseFuncName, attacker, target, skill, modifier, skillHitResult);
+		CallForPassiveSkills(baseFuncName, attacker, target, skill, modifier, skillHitResult);
+		CallForAbilities(baseFuncName, attacker, target, skill, modifier, skillHitResult);
 	}
 
 	/// <summary>
@@ -270,6 +277,72 @@ public class CombatModifierCalculationsScript : GeneralScript
 					func(attacker, target, skill, modifier, skillHitResult);
 				}
 			}
+		}
+	}
+
+	/// <summary>
+	/// Calls the given scriptable function for all active buffs (new system).
+	/// </summary>
+	private void CallForBuffs(string baseFuncName, ICombatEntity attacker, ICombatEntity target, Skill skill, SkillModifier modifier, SkillHitResult skillHitResult)
+	{
+		var buffIds = new HashSet<BuffId>();
+
+		if (attacker.Components.TryGet<BuffComponent>(out var buffs))
+			buffIds.UnionWith(buffs.GetList().Select(b => b.Id));
+
+		if (target.Components.TryGet<BuffComponent>(out buffs))
+			buffIds.UnionWith(buffs.GetList().Select(b => b.Id));
+
+		foreach (var buffId in buffIds)
+		{
+			var funcName = baseFuncName + "_" + buffId;
+
+			if (ScriptableFunctions.CombatModifier.TryGet(funcName, out var func))
+				func(attacker, target, skill, modifier, skillHitResult);
+		}
+	}
+
+	/// <summary>
+	/// Calls the given scriptable function for all passive skills (new system).
+	/// </summary>
+	private void CallForPassiveSkills(string baseFuncName, ICombatEntity attacker, ICombatEntity target, Skill skill, SkillModifier modifier, SkillHitResult skillHitResult)
+	{
+		var skillIds = new HashSet<SkillId>();
+
+		if (attacker.Components.TryGet<SkillComponent>(out var skills))
+			skillIds.UnionWith(skills.GetList(a => a.IsPassive).Select(s => s.Id));
+
+		if (target.Components.TryGet<SkillComponent>(out skills))
+			skillIds.UnionWith(skills.GetList(a => a.IsPassive).Select(s => s.Id));
+
+		foreach (var skillId in skillIds)
+		{
+			var funcName = baseFuncName + "_" + skillId;
+
+			if (ScriptableFunctions.CombatModifier.TryGet(funcName, out var func))
+				func(attacker, target, skill, modifier, skillHitResult);
+		}
+	}
+
+	/// <summary>
+	/// Calls the given scriptable function for all active abilities (new system).
+	/// </summary>
+	private void CallForAbilities(string baseFuncName, ICombatEntity attacker, ICombatEntity target, Skill skill, SkillModifier modifier, SkillHitResult skillHitResult)
+	{
+		var abilityIds = new HashSet<AbilityId>();
+
+		if (attacker.Components.TryGet<AbilityComponent>(out var abilities))
+			abilityIds.UnionWith(abilities.GetList(a => a.Active).Select(a => a.Id));
+
+		if (target.Components.TryGet<AbilityComponent>(out abilities))
+			abilityIds.UnionWith(abilities.GetList(a => a.Active).Select(a => a.Id));
+
+		foreach (var abilityId in abilityIds)
+		{
+			var funcName = baseFuncName + "_" + abilityId;
+
+			if (ScriptableFunctions.CombatModifier.TryGet(funcName, out var func))
+				func(attacker, target, skill, modifier, skillHitResult);
 		}
 	}
 
