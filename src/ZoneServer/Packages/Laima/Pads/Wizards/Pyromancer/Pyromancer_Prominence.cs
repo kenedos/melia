3b5c17d4c8235e68e9691796340a5408258ce1aa
kenedos <mrshadowccg@hotmail.com>
2026-03-25 10:35:36 -0300

    Change prominence into a pad

---
+++ src/ZoneServer/Packages/Laima/Pads/Wizards/Pyromancer/Pyromancer_Prominence.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using Melia.Shared.Packages;
+using Melia.Shared.Game.Const;
+using Melia.Shared.World;
+using Melia.Zone.Network;
+using Melia.Zone.Skills;
+using Melia.Zone.Skills.Combat;
+using Melia.Zone.Skills.SplashAreas;
+using Melia.Zone.World.Actors;
+using Melia.Zone.World.Actors.Monsters;
+using Melia.Zone.World.Actors.Pads;
+using Yggdrasil.Util;
+using static Melia.Zone.Skills.SkillUseFunctions;
+
+namespace Melia.Zone.Pads.Handlers
+{
+	[Package("laima")]
+	[PadHandler(PadName.prominence)]
+	public class Pyromancer_ProminenceOverride : ICreatePadHandler, IDestroyPadHandler, IUpdatePadHandler
+	{
+		private const int SimultaneousSalamanders = 5;
+		private const float EffectRadius = 150f;
+		private const float DamageWidth = 40f;
+		private const int MaxTargetsPerSalamander = 4;
+
+		public void Created(object sender, PadTriggerArgs args)
+		{
+			var pad = args.Trigger;
+			var creator = args.Creator;
+
+			Send.ZC_NORMAL.PadUpdate(creator, pad, true);
+			pad.SetRange(EffectRadius);
+			pad.SetUpdateInterval(1000);
+			pad.Trigger.LifeTime = TimeSpan.FromMilliseconds(10000);
+			pad.Trigger.MaxActorCount = 20;
+		}
+
+		public void Destroyed(object sender, PadTriggerArgs args)
+		{
+			var pad = args.Trigger;
+			var creator = args.Creator;
+
+			Send.ZC_NORMAL.PadUpdate(creator, pad, false);
+		}
+
+		public void Updated(object sender, PadTriggerArgs args)
+		{
+			var pad = args.Trigger;
+			var creator = args.Creator;
+			var skill = pad.Skill;
+
+			if (creator?.Map == null)
+				return;
+
+			for (var j = 0; j < SimultaneousSalamanders; j++)
+			{
+				var start = GetRandomPerimeterPosition(pad.Position, EffectRadius);
+				var end = GetRandomPerimeterPosition(pad.Position, EffectRadius);
+				this.CreateSalamanderEffect(creator, skill, start, end);
+			}
+		}
+
+		private void CreateSalamanderEffect(ICombatEntity caster, Skill skill, Position start, Position end)
+		{
+			var handleId = ZoneServer.Instance.World.CreateSkillHandle();
+			Send.ZC_NORMAL.Skill_124(caster, handleId, "I_wizard_Prominence_force_fire3", 1.2f, "F_wizard_prominence_fire", 1, 90, 5, DamageWidth, 0.35f, 5, 0.35f, start, 2);
+			Send.ZC_NORMAL.Skill_124(caster, handleId, start, end);
+
+			this.ApplyLineDamage(caster, skill, start, end);
+		}
+
+		private void ApplyLineDamage(ICombatEntity caster, Skill skill, Position start, Position end)
+		{
+			var direction = start.GetDirection(end);
+			var distance = start.Get2DDistance(end);
+			var rectangle = new Square(start, direction, (float)distance, DamageWidth);
+
+			var targets = caster.Map.GetAttackableEnemiesIn(caster, rectangle);
+			var hits = new List<SkillHitInfo>();
+			var hitCount = 0;
+
+			foreach (var target in targets)
+			{
+				if (hitCount >= MaxTargetsPerSalamander)
+					break;
+
+				var skillHitResult = SCR_SkillHit(caster, target, skill);
+				target.TakeDamage(skillHitResult.Damage, caster);
+
+				var skillHit = new SkillHitInfo(caster, target, skill, skillHitResult, TimeSpan.Zero, TimeSpan.Zero);
+				hits.Add(skillHit);
+
+				target.StartBuff(BuffId.Fire, skill.Level, skillHitResult.Damage, TimeSpan.FromSeconds(3), caster);
+				hitCount++;
+			}
+
+			Send.ZC_SKILL_HIT_INFO(caster, hits);
+		}
+
+		private static Position GetRandomPerimeterPosition(Position center, float radius)
+		{
+			var angle = RandomProvider.Get().NextDouble() * Math.PI * 2;
+			var x = center.X + radius * (float)Math.Cos(angle);
+			var z = center.Z + radius * (float)Math.Sin(angle);
+			return new Position(x, center.Y, z);
+		}
+	}
+}
--- src/ZoneServer/Packages/Laima/Skills/Wizards/Pyromancer/Pyromancer_Prominence.cs
+++ src/ZoneServer/Packages/Laima/Skills/Wizards/Pyromancer/Pyromancer_Prominence.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Melia.Shared.Packages;
 using Melia.Shared.Game.Const;
 using Melia.Shared.L10N;
@@ -8,11 +6,9 @@ using Melia.Shared.World;
 using Melia.Zone.Network;
 using Melia.Zone.Skills.Combat;
 using Melia.Zone.Skills.Handlers.Base;
-using Melia.Zone.Skills.SplashAreas;
-using Melia.Zone.World.Maps;
 using Melia.Zone.World.Actors;
-using Yggdrasil.Util;
 using static Melia.Zone.Skills.SkillUseFunctions;
+using static Melia.Zone.Skills.Helpers.SkillDamageHelper;
 
 namespace Melia.Zone.Skills.Handlers.Pyromancer
 {
@@ -23,12 +19,6 @@ namespace Melia.Zone.Skills.Handlers.Pyromancer
 	[SkillHandler(SkillId.Pyromancer_Prominence)]
 	public class Pyromancer_ProminenceOverride : IMeleeGroundSkillHandler, IDynamicCasted
 	{
-		private const int SalamanderCount = 10;
-		private const int SimultaneousSalamanders = 5;
-		private const float EffectRadius = 150f;
-		private const float DamageWidth = 40f;
-		private const int MaxTargetsPerSalamander = 4;
-
 		public void Handle(Skill skill, ICombatEntity caster, Position originPos, Position farPos, params ICombatEntity[] targets)
 		{
 			if (!skill.Vars.TryGet<Position>("Melia.ToolGroundPos", out var targetPos))
@@ -52,83 +42,14 @@ namespace Melia.Zone.Skills.Handlers.Pyromancer
 			skill.IncreaseOverheat();
 			caster.SetAttackState(true);
 
-			this.InitiateSkill(skill, caster, targetPos);
-			Send.ZC_SKILL_MELEE_GROUND(caster, skill, targetPos, null);
-			this.SpawnSalamanders(skill, caster, targetPos);
-		}
-
-		private void InitiateSkill(Skill skill, ICombatEntity caster, Position position)
-		{
 			var skillHandle = ZoneServer.Instance.World.CreateSkillHandle();
 
-			Send.ZC_NORMAL.RunPad(caster, skill, "prominence", position, caster.Direction, -1, 25, skillHandle, 0);
-			Send.ZC_SKILL_READY(caster, skill, skillHandle, caster.Position, position);
+			Send.ZC_NORMAL.RunPad(caster, skill, "prominence", targetPos, caster.Direction, -1, 25, skillHandle, 0);
+			Send.ZC_SKILL_READY(caster, skill, skillHandle, caster.Position, targetPos);
 			Send.ZC_NORMAL.UpdateSkillEffect(caster, caster.Handle, caster.Position, caster.Direction, caster.Position);
-		}
+			Send.ZC_SKILL_MELEE_GROUND(caster, skill, targetPos, null);
 
-		private void SpawnSalamanders(Skill skill, ICombatEntity caster, Position position)
-		{
-			for (var i = 0; i < SalamanderCount; i++)
-			{
-				skill.Wait(TimeSpan.FromMilliseconds(1000 * (i + 1))).ContinueWith(_ =>
-				{
-					if (caster?.Map == null)
-						return;
-
-					for (var j = 0; j < SimultaneousSalamanders; j++)
-					{
-						var start = this.GetRandomPerimeterPosition(caster.Map, position, EffectRadius);
-						var end = this.GetRandomPerimeterPosition(caster.Map, position, EffectRadius);
-						this.CreateSalamanderEffect(caster, skill, start, end);
-
-					}
-				});
-			}
-		}
-
-		private void CreateSalamanderEffect(ICombatEntity caster, Skill skill, Position start, Position end)
-		{
-			var handleId = ZoneServer.Instance.World.CreateSkillHandle();
-			Send.ZC_NORMAL.Skill_124(caster, handleId, "I_wizard_Prominence_force_fire3", 1.2f, "F_wizard_prominence_fire", 1, 90, 5, DamageWidth, 0.35f, 5, 0.35f, start, 2);
-			Send.ZC_NORMAL.Skill_124(caster, handleId, start, end);
-
-			this.ApplyLineDamage(caster, skill, start, end);
-		}
-
-		private void ApplyLineDamage(ICombatEntity caster, Skill skill, Position start, Position end)
-		{
-			var direction = start.GetDirection(end);
-			var distance = start.Get2DDistance(end);
-			var rectangle = new Square(start, direction, (float)distance, DamageWidth);
-
-			var targets = caster.Map.GetAttackableEnemiesIn(caster, rectangle);
-			var hits = new List<SkillHitInfo>();
-			var hitCount = 0;
-
-			foreach (var target in targets)
-			{
-				if (hitCount >= MaxTargetsPerSalamander)
-					break;
-
-				var skillHitResult = SCR_SkillHit(caster, target, skill);
-				target.TakeDamage(skillHitResult.Damage, caster);
-
-				var skillHit = new SkillHitInfo(caster, target, skill, skillHitResult, TimeSpan.Zero, TimeSpan.Zero);
-				hits.Add(skillHit);
-
-				target.StartBuff(BuffId.Fire, skill.Level, skillHitResult.Damage, TimeSpan.FromSeconds(3), caster);
-				hitCount++;
-			}
-
-			Send.ZC_SKILL_HIT_INFO(caster, hits);
-		}
-
-		private Position GetRandomPerimeterPosition(Map map, Position center, float radius)
-		{
-			var angle = RandomProvider.Get().NextDouble() * Math.PI * 2;
-			var x = center.X + radius * (float)Math.Cos(angle);
-			var z = center.Z + radius * (float)Math.Sin(angle);
-			return new Position(x, center.Y, z);
+			SkillCreatePad(caster, skill, targetPos, 0f, PadName.prominence);
 		}
 	}
 }
