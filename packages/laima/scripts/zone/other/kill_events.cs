69a68536163dc898c1f3decd94f3bfc9bf8a168b
kenedos <mrshadowccg@hotmail.com>
2026-04-19 00:38:28 -0300

    Companion benefit root crystal

--- packages/laima/scripts/zone/other/kill_events.cs
+++ packages/laima/scripts/zone/other/kill_events.cs
@@ -5,6 +5,8 @@
 //---------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Melia.Shared.Game.Const;
 using Melia.Shared.Scripting;
@@ -14,6 +16,7 @@ using Melia.Zone.Scripting;
 using Melia.Zone.World.Actors;
 using Melia.Zone.World.Actors.Characters;
 using Melia.Zone.World.Actors.Monsters;
+using Yggdrasil.Geometry.Shapes;
 using static Melia.Shared.Util.TaskHelper;
 
 public class KillEventsScript : GeneralScript
@@ -27,22 +30,40 @@ public class KillEventsScript : GeneralScript
 
 	private void OnRootCrystalKilled(Mob mob, ICombatEntity attacker)
 	{
-		attacker.StartBuff(BuffId.RootCrystalMoveSpeed, 10, 0, TimeSpan.FromSeconds(15), attacker);
+		var duration = TimeSpan.FromSeconds(15);
+		var applied = new HashSet<ICombatEntity>();
 
-		if (attacker is Character character)
+		void ApplyBuff(ICombatEntity target)
 		{
-			CallSafe(this.MonsterHealStamina(mob, character, 100000));
+			if (target == null || target.IsDead || !applied.Add(target))
+				return;
+			target.StartBuff(BuffId.RootCrystalMoveSpeed, 10, 0, duration, attacker);
+		}
 
-			var party = character.Connection?.Party;
-			if (party != null)
+		ApplyBuff(attacker);
+
+		var character = mob.GetKillBeneficiary(attacker);
+		if (character == null)
+			return;
+
+		ApplyBuff(character);
+		CallSafe(this.MonsterHealStamina(mob, character, 100000));
+
+		var recipients = new List<Character> { character };
+		if (character.Connection?.Party != null)
+			recipients.AddRange(character.Map.GetPartyMembersInRange(character, 150).Where(m => m != character));
+
+		foreach (var recipient in recipients)
+		{
+			if (recipient != character)
 			{
-				var members = character.Map.GetPartyMembersInRange(character, 150);
-				foreach (var member in members)
-				{
-					member.StartBuff(BuffId.RootCrystalMoveSpeed, 10, 0, TimeSpan.FromSeconds(15), attacker);
-					CallSafe(this.MonsterHealStamina(mob, member, 100000));
-				}
+				ApplyBuff(recipient);
+				CallSafe(this.MonsterHealStamina(mob, recipient, 100000));
 			}
+
+			var area = new CircleF(recipient.Position, 150);
+			foreach (var minion in recipient.Map.GetAliveAlliedEntitiesIn(recipient, area).Where(e => e is Summon || e is Companion))
+				ApplyBuff(minion);
 		}
 	}
 
